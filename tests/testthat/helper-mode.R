d0317a526103aad5d38425258e163586b8b34f8d
Alex <alex.hill@gmail.com>
2022-03-04 15:26:13 +0000

    add test against dde

--- tests/testthat/helper-mode.R
+++ tests/testthat/helper-mode.R
@@ -1,9 +1,24 @@
 logistic_analytic <- function(r, K, end_time, y0) {
-  sapply(0:end_time, function(ti)  K / (1 + (K / y0 - 1) * exp(-r * ti)))
+  sapply(0:end_time, function(t) K / (1 + (K / y0 - 1) * exp(-r * t)))
 }
 
 logistic_dopri <- function(r, K, end_time, y0) {
   res <- solve_logistic(r, K, end_time, y0)
   dim(res) <- c(length(r), length(res) / 2)
   res
+}
+
+logistic_rhs <- function(t, y0, parms) {
+  K <- parms$K
+  r <- parms$r
+  r * y0 * (1 - y0 / K)
+}
+
+logistic_dde <- function(r, K, end_time, y0) {
+  tt <- 0:end_time
+  dde::dopri(y0, tt, logistic_rhs,
+             list(r = r, K = K),
+             tcrit = tt,
+             return_time = FALSE,
+             return_by_column = FALSE)
 }
--- tests/testthat/test-dopri.R
+++ tests/testthat/test-dopri.R
@@ -7,5 +7,17 @@ test_that("can integrate logistic", {
   expected <- logistic_analytic(r, K, end_time, y0)
   actual <- logistic_dopri(r, K, end_time, y0)
 
-  expect_equal(expected, actual, tolerance = 0.01)
+  expect_equal(expected, actual, tolerance = 1e-7)
+})
+
+test_that("integration agrees with dde", {
+  y0 <- c(1, 1)
+  r <- c(0.1, 0.2)
+  K <- c(100, 100)
+  end_time <- 25
+
+  expected <- logistic_dde(r, K, end_time, y0)
+  actual <- logistic_dopri(r, K, end_time, y0)
+
+  expect_equal(expected, actual)
 })
