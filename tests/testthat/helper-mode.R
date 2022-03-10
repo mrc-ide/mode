e70f6d175bfb5a1ee88e513f4b876957e5f6e1f4
Alex <alex.hill@gmail.com>
2022-03-10 15:08:25 +0000

    improve test

--- tests/testthat/helper-mode.R
+++ tests/testthat/helper-mode.R
@@ -2,14 +2,6 @@ logistic_analytic <- function(r, K, times, y0) {
   sapply(times, function(t) K / (1 + (K / y0 - 1) * exp(-r * t)))
 }
 
-logistic_mode <- function(r, K, times, y0) {
-  path <- mode_file("examples/logistic.cpp")
-  gen <- mode(path, quiet = TRUE)
-  pars <- list(r1 = r[[1]], r2 = r[[2]], K1 = K[[1]], K2 = K[[2]])
-  mod <- gen$new(pars, 0)
-  sapply(times, function(t) mod$solve(t))
-}
-
 logistic_rhs <- function(t, y0, parms) {
   K <- parms$K
   r <- parms$r
--- tests/testthat/test-dopri.R
+++ tests/testthat/test-dopri.R
@@ -7,7 +7,13 @@ test_that("can integrate logistic", {
   analytic <- logistic_analytic(r, K, times, y0)
   dde <- logistic_dde(r, K, times, y0)
 
-  actual <- logistic_mode(r, K, times, y0)
+  path <- mode_file("examples/logistic.cpp")
+  gen <- mode(path, quiet = TRUE)
+  pars <- list(r1 = r[[1]], r2 = r[[2]], K1 = K[[1]], K2 = K[[2]])
+  mod <- gen$new(pars, 0)
+
+  actual <- vapply(times, function(t) mod$solve(t), numeric(length(y0)))
   expect_equal(actual, analytic, tolerance = 1e-7)
   expect_equal(actual, dde, tolerance = 1e-7)
+  expect_equal(mod$time(), 25)
 })
