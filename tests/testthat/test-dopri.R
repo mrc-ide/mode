8da100cf57a89d3aace305878779615ad98cf4ad
Alex <alex.hill@gmail.com>
2022-03-10 18:30:30 +0000

    extend test

--- tests/testthat/test-dopri.R
+++ tests/testthat/test-dopri.R
@@ -10,10 +10,17 @@ test_that("can integrate logistic", {
   path <- mode_file("examples/logistic.cpp")
   gen <- mode(path, quiet = TRUE)
   pars <- list(r1 = r[[1]], r2 = r[[2]], K1 = K[[1]], K2 = K[[2]])
-  mod <- gen$new(pars, 0, 1)
+  n_particles <- 5
+  mod <- gen$new(pars, 0, n_particles)
 
-  actual <- vapply(times, function(t) mod$solve(t), numeric(length(y0)))
-  expect_equal(actual, analytic, tolerance = 1e-7)
-  expect_equal(actual, dde, tolerance = 1e-7)
+  actual <- vapply(times, function(t) mod$solve(t), numeric(length(y0) * n_particles))
+  expect_equal(actual[1:2,], analytic, tolerance = 1e-7)
+  expect_equal(actual[1:2,], dde, tolerance = 1e-7)
   expect_equal(mod$time(), 25)
+
+  # all particles should have the same values
+  y0 <- actual[c(TRUE, FALSE),]
+  y1 <- actual[c(FALSE, TRUE),]
+  expect_equal(nrow(unique(y0)), 1)
+  expect_equal(nrow(unique(y1)), 1)
 })
