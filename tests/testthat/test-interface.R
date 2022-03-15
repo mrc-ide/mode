cf4147b7ee8da8a27e8c5cd311194666ebf6c80b
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2022-03-15 14:57:50 +0000

    Quieten output during tests

--- tests/testthat/test-interface.R
+++ tests/testthat/test-interface.R
@@ -1,6 +1,6 @@
 test_that("Can compile a simple model", {
   path <- mode_file("examples/logistic.cpp")
-  gen <- mode(path, quiet = FALSE)
+  gen <- mode(path, quiet = TRUE)
   pars <- list(r1 = 0.1, r2 = 0.2, K1 = 100, K2 = 100)
   n_particles <- 10
   mod <- gen$new(pars, pi, n_particles)
